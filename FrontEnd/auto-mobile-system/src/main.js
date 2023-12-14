3a77e12db34875d53da397ae9fa8d0abf8b4903b
zain0456 <zain.ul.abideen1565@gmail.com>
2023-12-14 22:57:36 +0500

    Update main.js

--- FrontEnd/auto-mobile-system/src/main.js
+++ FrontEnd/auto-mobile-system/src/main.js
@@ -5,7 +5,7 @@ import router from './router'
 // import {Vue} from 'vue';
 import '../src/styles.css'
 import axios from 'axios';
-axios.defaults.baseURL = 'https://backend-car-rental-project.pages.dev/';
+axios.defaults.baseURL = 'http://localhost:3001';
 axios.defaults.headers = {
     'Headers' : store.state.user.tokens,
     'Content-type' : 'application/json'
